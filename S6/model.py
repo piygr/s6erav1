a175b408299d8dfd88f03b83aa88df76fa2017cc
Piyush Grover <grover1piyush@gmail.com>
2023-06-07 01:02:50 +0530

    model.py

--- S6/model.py
+++ S6/model.py
@@ -8,53 +8,62 @@ class OptimizedNet(nn.Module):
         super(OptimizedNet, self).__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 8, 3, padding=1), #28x28x
+            nn.Conv2d(1, 8, 3, padding=1), #=> 8x28x28
             nn.BatchNorm2d(8),
             nn.ReLU(),
             nn.Dropout2d(0.02),
-            nn.Conv2d(8, 8, 3, padding=1),
+            nn.Conv2d(8, 8, 3, padding=1), #=> 8x28x28
             nn.BatchNorm2d(8),
             nn.ReLU(),
             nn.Dropout2d(0.02),
-            nn.MaxPool2d(2, 2)
+            nn.MaxPool2d(2, 2)             #=> 8x14x14
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(8, 16, 3, padding=1), #14x14x16
+            nn.Conv2d(8, 16, 3, padding=1), # => 16x14x14
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Dropout2d(0.02),
-            nn.Conv2d(16, 16, 3, padding=1),    #14x14x16
+            nn.Conv2d(16, 16, 3, padding=1),    # => 16x14x14
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Dropout2d(0.02)
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(16, 32, 3),    #5x5x32
+            nn.Conv2d(16, 32, 3),    # => 32x12x12
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout2d(0.02),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 32, 3),  # 5x5x32
+            nn.MaxPool2d(2, 2),      # => 32x6x6
+            nn.Conv2d(32, 32, 3),    # => 32x4x4
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout2d(0.02)
         )
-        self.conv_linear = nn.Conv2d(32, 10, 1)
+        self.conv_linear = nn.Conv2d(32, 10, 1) # => 10x4x4
 
 
     def forward(self, x):
 
+        #print(x.size())         # => torch.Size([<2>, 1, 28, 28])
         x = self.conv1(x)
 
+        #print(x.size())         # => torch.Size([<2>, 8, 14, 14])
         x = self.conv2(x)
 
+        #print(x.size())         # => torch.Size([<2>, 16, 14, 14])
         x = self.conv3(x)
 
+        #print(x.size())         # => torch.Size([<2>, 32, 4, 4])
         x = self.conv_linear(x)
 
-        x = F.avg_pool2d(x, 4)
+        #print(x.size())         # => torch.Size([<2>, 10, 4, 4])
+        x = F.avg_pool2d(x, 4)          # => 10x1x1
 
+        #print(x.size())         # => torch.Size([<2>, 10, 1, 1])
         x = x.view(x.size(0), 10)
+
+        #print(x.size())  # => torch.Size([<2>, 10])
+
         return F.log_softmax(x, dim=1)
